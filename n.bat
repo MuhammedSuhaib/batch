485d452f086d7da4f653f213ce0a72fbe58bc679
Muhammad Suhaib <154515857+MuhammedSuhaib@users.noreply.github.com>
2025-03-30 01:02:20 +0500

    Add files via upload

---
+++ n.bat
@@ -0,0 +1,114 @@
+@REM @echo off
+@REM :loop
+@REM cls
+@REM echo                                           "o"   
+@REM echo                                          "/|\"
+@REM echo                                          "/ \"
+@REM timeout /t 1 >nul
+@REM cls
+@REM @REM echo.
+@REM echo                                  "o"  
+@REM echo                                 "/|\"
+@REM echo                                 "/ \"
+@REM timeout /t 1 >nul
+@REM cls
+@REM @REM echo.
+@REM set /a colorNum=%random% %% 7 + 1
+@REM color %colorNum%
+@REM echo                         "o"    
+@REM echo                        "/|\"
+@REM echo                        "/ \"                              [91m            "(;@=_-o_-O_-o_-O_-o_"[0m
+@REM timeout /t 1 >nul
+@REM cls
+@REM echo                "o"      
+@REM echo               "/|\"
+@REM echo               "/ \"                  [91m  "(;@=_-o_-O_-o_-O_-o_"[0m
+
+@REM @REM timeout /t 1 >nul
+@REM @REM cls
+@REM @REM echo     "o"
+@REM @REM echo    "/|\"   [91m "(;@=" [0m_    
+@REM @REM echo    "/ \"        [91m "&" [0m
+@REM @REM echo                    [91m "-O*-O_-o_-O_-o_"[0m
+
+@REM @REM timeout /t 1 >nul
+@REM @REM cls
+@REM @REM echo "o"  
+@REM @REM echo "|\"
+@REM @REM echo " \"
+@REM @REM timeout /t 1 >nul
+@REM @REM cls
+@REM @REM echo.
+@REM @REM echo       "o" 
+@REM @REM echo      "/|\"
+@REM @REM echo      "/ \"
+@REM @REM timeout /t 1 >nul
+@REM @REM cls
+@REM @REM echo.
+@REM @REM echo.
+@REM @REM echo                   "o"
+@REM @REM echo                  "/|\"
+@REM @REM echo                  "/ \"
+@REM @REM timeout /t 1 >nul
+@REM @REM cls
+@REM @REM echo.
+@REM @REM echo.
+@REM @REM echo.
+@REM @REM echo                            "o"
+@REM @REM echo                           "/|\"
+@REM @REM echo                           "/ \"
+@REM @REM timeout /t 1 >nul
+@REM @REM cls
+@REM @REM echo.
+@REM @REM echo.
+@REM @REM echo.
+@REM @REM echo.
+@REM @REM echo.
+@REM @REM echo                                           "o"
+@REM @REM echo                                          "/|\"
+@REM @REM echo                                          "/ \"
+@REM @REM timeout /t 1 >nul
+@REM goto loop
+@echo off
+:loop
+set /a colorNum=%random% %% 7 + 1
+color %colorNum%
+cls
+echo [5m Blinking Text [0m
+timeout /t 3 >nul
+cls
+echo [####        ] 50%%
+timeout /t 3 >nul
+cls
+echo [5m Blinking Text [0m
+cls
+timeout /t 3 >nul
+
+echo [5m Blinking Text [0m
+cls
+timeout /t 3 >nul
+
+echo [5m Blinking Text [0m
+cls
+timeout /t 3 >nul
+
+echo [5m Blinking Text [0m
+cls
+timeout /t 3 >nul
+
+echo [5m Blinking Text [0m
+cls
+timeout /t 3 >nul
+
+echo [5m Blinking Text [0m
+cls
+timeout /t 3 >nul
+
+echo [5m Blinking Text [0m
+cls
+timeout /t 3 >nul
+
+echo [5m Blinking Text [0m
+cls
+
+goto loop
